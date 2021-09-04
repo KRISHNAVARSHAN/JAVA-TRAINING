5bbc680de01b2ffda5cb106d9639837eb5afaa2f
KRISHNAVARSHAN <krishnavarshan28@gmail.com>
2021-09-04 13:09:33 +0530

    first commit

---
+++ java_training_1/ex2.java
@@ -0,0 +1,16 @@
+package java_training_1;
+
+public class ex2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean a = true;
+		byte b = 12;
+		int c= 25;
+		float d = 56.2f;
+		double e = 14.3;
+		char f = 'J';
+		System.out.println(a + " " + b + " " + c + " " + d + " " + e + " " +f);
+	}
+
+
+}
